bf63c90e143e4ad7a61fe801c420083d0daec4a3
abetolmunyat-boop <abetolmunyat@gmail.com>
2026-05-01 03:05:17 +0300

    Update config.js

--- config.js
+++ config.js
@@ -2,4 +2,4 @@
 // When you deploy your backend to Render, replace this with your live backend URL
 // Example: const API_BASE_URL = 'https://cosnurses-api.onrender.com';
 
-const API_BASE_URL = 'http://localhost:3000';
+const API_BASE_URL = 'https://jobs-go-abroad.onrender.com';
